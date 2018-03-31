8e4a07bd148cf8145f06fa9f776aea5d56fbdf5f
AaronStPete <AaronStPete@users.noreply.github.com>
2018-03-31 17:54:22 -0400

    Completed basic assignment with basic styling

--- public/main.js
+++ public/main.js
@@ -1,5 +1,15 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+const BASE_URL = "http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=57bf24c3942a509ae76819c5ba3f5fcc&format=json";
 
-document.addEventListener('DOMContentLoaded', main)
+angular
+  .module("mainApp", [])
+  .controller("mainController", ["$scope", "$http",($scope, $http) => {
+  
+    $http({
+    method: "GET",
+    url: BASE_URL,
+  }).then(response => {
+    console.log(response.data);
+    $scope.artists = response.data.artists.artist;
+    console.log($scope.artists);
+  })
+}])
